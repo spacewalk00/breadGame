74397ebbb0ca88fb5e278ea3afb5b950d61a6b6e
dokyung-kang <kangdk02@naver.com>
2022-07-12 23:11:35 +0900

    second commit

---
+++ breadGame20220712/home.lua
@@ -0,0 +1,297 @@
+-----------------------------------------------------------------------------------------
+--
+-- view1.lua
+--
+-----------------------------------------------------------------------------------------
+
+local composer = require( "composer" )
+local scene = composer.newScene()
+
+syrub = 0
+ingredient = 0
+
+function moveToBook(event)
+	print("도감으로 이동")
+	------------showCoin관련 수정
+	showCoin.isVisible = false
+	composer.gotoScene("view1")
+end
+
+function moveToBreadRoom(event)
+	print("빵방으로 이동")
+	-------------showCoin 관련 수정
+	showCoin.isVisible = false
+	composer.gotoScene("part5")
+end
+---------------업적으로 이동---------------------
+local function  gotoAchieve(event)
+	print("업적으로 이동")
+	---------showCoin 관련 수정
+	showCoin.x, showCoin.y = display.contentWidth*0.54, display.contentHeight*0.053
+	composer.gotoScene("achieve")
+end
+------------------------------------------------
+----------------------- 재화 관련 -------------------------
+-----------showCoin 수정
+--showCoin = display.newText(coinNum, display.contentWidth*0.35, display.contentHeight*0.04, "Content/font/ONE Mobile POP.ttf", 50)
+--showCoin:setFillColor(0)
+--[[
+function forCoin()
+	showCoin = display.newText(coinNum, display.contentWidth*0.35, display.contentHeight*0.04, "Content/font/ONE Mobile POP.ttf", 50)
+	showCoin:setFillColor(0)
+end
+
+function deleteBeforeNum()
+	showCoin:removeSelf()
+end
+
+--forCoin()
+]]
+----------------- 경험치에 따른 레벨업 관련 ---------------------
+levelUpFlag = 0 
+local function levelUpPop( n ) --레벨업 창 화면에 띄우기 --
+	print("레벨업!!!!!!!!!"..n.."으로")
+	--audio.play( soundTable["levelUpSound"] ,  {channel=6})
+	
+	levelUpFlag = 1
+
+	levelGroup = display.newGroup()
+
+	levelDarkening = display.newImageRect(levelGroup, "Content/images/dark.png", 1440*2, 710*7)
+	levelDarkening.x, levelDarkening.y = display.contentCenterX, display.contentCenterY
+	
+	local levelUpPopMark = display.newImage(levelGroup, "Content/images/levelUp.png")
+	levelUpPopMark.x, levelUpPopMark.y = display.contentCenterX, display.contentCenterY*0.8
+	showNewLevel = display.newText(n, display.contentWidth*0.075, display.contentHeight*0.045, "Content/font/ONE Mobile POP.ttf", 100)
+	showNewLevel.x, showNewLevel.y = display.contentWidth*0.52, display.contentHeight*0.37
+	showNewLevel:setFillColor(1)
+
+	
+	levelGroup:insert(showNewLevel)
+
+	---deleteBeforeLevel()
+	---forLevel()
+	
+	local message = display.newText(levelGroup, "LEVEL UP!", display.contentCenterX, display.contentCenterY, "Content/font/ONE Mobile POP.ttf", 100)
+	message:setFillColor(1)
+
+	transition.fadeOut(levelGroup, {timer = 2000, delay = 1000})
+end
+--------------------------------showLevel 수정
+	showLevel = display.newText(levelNum, display.contentWidth*0.075, display.contentHeight*0.045, "Content/font/ONE Mobile POP.ttf", 90)
+	showLevel:setFillColor(1)
+	--[[
+function forLevel() -- 레벨 숫자 다루기--
+	showLevel = display.newText(levelNum, display.contentWidth*0.075, display.contentHeight*0.045, "Content/font/ONE Mobile POP.ttf", 90)
+	showLevel:setFillColor(1)
+end
+function deleteBeforeLevel() --레벨 숫자 다루기2--
+	showLevel:removeSelf()	
+end
+	]]
+
+function levelUp() --레벨업 --
+	print("레벨 몇이냐면..."..levelNum.."!!!!!!!")
+	for i=2, 10 do
+		if exp >= 1000 + (i-2)*1000 and levelFirstTime[i] == 1 then
+			levelNum = i
+			if i == 10 then
+				coinNum = coinNum + 10000
+			else 
+				coinNum = coinNum + 3000
+			end
+			levelFirstTime[i] = 0
+			levelUpPop(levelNum)
+			--재화 갱신--
+			---deleteBeforeNum()
+			---forCoin()
+			----------showCoin 수정
+			showCoin.text = coinNum
+			----------showLevel 수정
+			showLevel.text = levelNum
+			break
+		end
+	end
+end
+
+function scene:create( event )
+	local sceneGroup = self.view
+	--[[
+	-- 배경 --
+	local background = display.newImage("Content/image/main_image.png", display.contentWidth, display.contentHeight)
+	background.x, background.y = display.contentWidth/2, display.contentHeight/2]]
+
+	----------showCoin 수정
+	showCoin.text = coinNum
+	---forCoin() -- 재화 업뎃
+	-----------showLevel 수정
+	---forLevel() -- 레벨 업뎃
+	local background = display.newImage("Content/images/background.png")
+	background.x, background.y = display.contentCenterX, display.contentCenterY
+
+	local oven = display.newImageRect("Content/images/oven.png", 1550, 820)
+	oven.x, oven.y = display.contentCenterX, display.contentHeight*0.9
+
+	local level = display.newImage("Content/images/level.png")
+	level.x, level.y = display.contentWidth*0.07, display.contentHeight*0.04
+
+	--local showLevel = display.newText(levelNum, display.contentWidth*0.075, display.contentHeight*0.045, "Content/font/ONE Mobile POP.ttf", 90)
+	--showLevel:setFillColor(1)
+
+	local coins = display.newImage("Content/images/coins.png")
+	coins.x, coins.y = display.contentWidth*0.3, display.contentHeight*0.04
+
+	local book = display.newImage("Content/images/book.png");
+	book.x, book.y = display.contentWidth*0.75, display.contentHeight*0.04
+	local s_book = display.newImage("Content/images/shadow.png")
+	s_book.x, s_book.y = display.contentWidth*0.75, display.contentHeight*0.04
+
+	local store = display.newImageRect("Content/images/store.png", 170, 170)
+	store.x, store.y = display.contentWidth*0.9, display.contentHeight*0.035
+	local s_store = display.newImage("Content/images/shadow.png")
+	s_store.x, s_store.y = display.contentWidth*0.9, display.contentHeight*0.04
+
+	local function gotoStore(event)
+	    	--deleteBeforeNum()
+	    	--forCoin()
+	    -------------showCoin 관련 수정--------
+		showCoin.x, showCoin.y = display.contentWidth*0.55, display.contentHeight*0.05
+		composer.gotoScene("store_i")
+	end
+	store:addEventListener("tap", gotoStore)
+
+	
+	local success = display.newImage("Content/images/success.png")
+	success.x, success.y = display.contentWidth*0.75, display.contentHeight*0.13
+	local s_success = display.newImage("Content/images/shadow.png")
+	s_success.x, s_success.y = display.contentWidth*0.75, display.contentHeight*0.13
+
+	local breadRoom = display.newImageRect("Content/images/breadRoom.png", 170, 170)
+	breadRoom.x, breadRoom.y = display.contentWidth*0.9, display.contentHeight*0.135
+	local s_breadRoom = display.newImage("Content/images/shadow.png")
+	s_breadRoom.x, s_breadRoom.y = display.contentWidth*0.9, display.contentHeight*0.13
+	
+	book:addEventListener("tap", moveToBook)
+	breadRoom:addEventListener("tap", moveToBreadRoom)
+	----------업적 관련 추가--------------
+	success:addEventListener("tap", gotoAchieve)
+	-------------------------------------
+	
+	--[[
+	----------엔딩보기 추가---------------
+	local ending = display.newText("엔딩보기", display.contentWidth*0.95, display.contentHeight*0.99, "font/ONE Mobile POP.ttf", 40)
+	--ending:setFillColor(0.5)
+	function gotoEnd(event)
+		----------------showCoin관련 수정
+		showCoin.isVisible = false
+		composer.gotoScene("outtro")
+	end
+	ending:addEventListener("tap", gotoEnd)
+	]]
+	
+	-------------------------------------
+	--재료 추가 버튼--
+	local material_button = display.newImage("Content/image/material_button.png", display.contentWidth, display.contentHeight)
+	material_button.x, material_button.y = display.contentWidth * 0.138, display.contentHeight * 0.444
+
+	local function plus( event )
+ 
+	    if ( event.phase == "began" ) then
+	        composer.gotoScene("syrup")
+		end
+	end
+	material_button:addEventListener("touch", plus)
+
+
+	sceneGroup:insert( background )
+
+	sceneGroup:insert( oven )
+	sceneGroup:insert( level )
+	sceneGroup:insert( showLevel )
+	sceneGroup:insert( coins ) 
+	sceneGroup:insert( s_book ) sceneGroup:insert( book ) 
+	sceneGroup:insert( s_store ) sceneGroup:insert( store ) 
+	sceneGroup:insert( s_success ) sceneGroup:insert( success ) 
+	sceneGroup:insert( s_breadRoom ) sceneGroup:insert( breadRoom ) 
+	sceneGroup:insert( material_button )
+
+	-------------------엔딩보기------------
+	--빵 도감 채운 개수 전역변수
+	breadBook_count = 0
+	--업그레이드 빵 도감 채운 개수
+	breadBookUP_count = 0
+	for n = 1, 4 do
+		for m = 1, 8 do	
+			if openBread[n][m] ~= 0 then
+				breadBook_count = breadBook_count + 1
+			end
+		end
+	end
+	for n = 1, 4 do
+		for m = 1, 8 do
+			if openUBread[n][m] ~= 0 then
+				breadBookUP_count = breadBookUP_count + 1
+			end
+		end
+	end
+	if breadBook_count + breadBookUP_count >= 63 then 
+		local endingButton = display.newImage("Content/images/아웃트로/togoEnd.png", display.contentWidth*0.88, display.contentHeight*0.19)
+		local ending = display.newText("엔딩보기", display.contentWidth*0.88, display.contentHeight*0.19, "font/ONE Mobile POP.ttf", 40)
+		--ending:setFillColor(0.5)
+
+		function gotoEnding(event)
+		----------------showCoin관련 수정
+			showCoin.isVisible = false
+			composer.gotoScene("outtro")
+		end
+		endingButton:addEventListener("tap", gotoEnding)
+		sceneGroup:insert(endingButton)
+		sceneGroup:insert(ending)
+	end
+	-------------------------------------
+end
+
+function scene:show( event )
+	local sceneGroup = self.view
+	local phase = event.phase
+	
+	if phase == "will" then
+		-- Called when the scene is still off screen and is about to move on screen
+	elseif phase == "did" then
+		-- Called when the scene is now on screen
+		-- 
+		-- INSERT code here to make the scene come alive
+		-- e.g. start timers, begin animation, play audio, etc.
+	end	
+end
+
+
+function scene:hide( event )
+	local sceneGroup = self.view
+	local phase = event.phase
+	
+	if event.phase == "will" then
+	elseif phase == "did" then
+		composer.removeScene("home")
+		composer.removeScene("result")
+	end
+end
+
+function scene:destroy( event )
+	local sceneGroup = self.view
+	-- Called prior to the removal of scene's "view" (sceneGroup)
+	-- 
+	-- INSERT code here to cleanup the scene
+	-- e.g. remove display objects, remove touch listeners, save state, etc.
+end
+
+---------------------------------------------------------------------------------
+
+-- Listener setup
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+-----------------------------------------------------------------------------------------
+
+return scene
---
+++ breadGame20220712/intro.lua
@@ -0,0 +1,230 @@
+-----------------------------------------------------------------------------------------
+--
+-- intro.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- JSON 파싱
+local json = require('json')
+
+local Data, pos, msg
+
+local function parse()
+	local filename = system.pathForFile("Content/JSON/intro.json")
+	Data, pos, msg = json.decodeFile(filename)
+
+	--디버그
+	if Data then
+		print(Data[1].type)
+	else
+		print(pos)
+		print(msg)
+	end
+	--
+end
+parse()
+--
+
+-------업적 전역 변수----------
+--돈 전역 변수
+coinNum = 0
+--폭탄빵 제작 전역변수
+bomb_count = 0
+--연속 일반빵 제작 전역변수
+breadNormal_count = 0
+--폭탄빵 연속 제작 전역변수
+breadBomb_count = 0
+--빵 제작 전역변수
+bread_count = 0
+--빵 도감 채운 개수 전역변수
+breadBook_count = 0
+--업그레이드 빵 도감 채운 개수
+breadBookUP_count = 0
+--빵 방 빵 채운 개수 전역변수
+breadRoom_count = 0
+
+--돈 표시--
+showCoin = display.newText(coinNum, display.contentWidth*0.35, display.contentHeight*0.04, "Content/font/ONE Mobile POP.ttf", 50)
+showCoin:setFillColor(0)
+showCoin.isVisible = false
+
+
+
+local composer = require( "composer" )
+local scene = composer.newScene()
+
+function scene:create( event )
+	local sceneGroup = self.view
+	local windowGroup = display.newGroup()
+
+	--배경화면
+	local background = display.newRect(display.contentWidth/2, display.contentHeight/2, display.contentWidth, display.contentHeight)
+	--skip 버튼
+	local skip_B = display.newText("SKIP", display.contentWidth*0.92, display.contentHeight*0.04, "Content/font/ONE Mobile POP.ttf")
+	skip_B.size = 50
+	skip_B:setFillColor(0)
+	--말풍선
+	local bubble = display.newImage("Content/images/인트로/bubble.png", display.contentWidth/2, display.contentHeight*0.86)
+
+	--local name = display.newImage("Content/images/인트로/name.png", display.contentWidth*0.1, display.contentHeight*0.8, display.contentWidth, display.contentHeight)
+	--클릭 버튼
+	local click = display.newImage("Content/images/인트로/click.png", bubble.x + 550, bubble.y + 165)
+	--대사
+	local script = display.newText("더미 텍스트", bubble.x, bubble.y-20, "Content/font/ONE Mobile POP.ttf")
+	--script.width = display.contentWidth*0.6
+	script.size = 65
+	script:setFillColor(0)
+
+	---------------skip하기--------------
+	local function skip(event)
+		local options = {
+			effect = "crossFade",
+			time = 200
+		}
+		showCoin.isVisible = true
+		composer.gotoScene("home", options)
+	end
+	skip_B:addEventListener("tap", skip)
+
+	------------------대사----------------
+	local index = 1
+	local function nextScript( ... )
+		local options = {
+			effect = "fade",
+			time = 200
+		}
+		if (index <= #Data) then
+			if( Data[index].type == "background" ) then
+				
+				--배경 바꾸기
+				background.fill = {
+					type = "image",
+					filename = Data[index].img
+				}
+				index = index + 1
+				nextScript()
+			
+			elseif( Data[index].type == "bubble" ) then
+			 
+			 	--말풍선
+				bubble.fill = {
+					type = "image",
+					filename = Data[index].img
+				}
+				index = index + 1
+				nextScript()
+			
+			elseif( Data[index].type == "Narration" ) then
+			 
+			 	--해설
+				 script.text = Data[index].content
+				 index = index + 1
+			end
+		else	
+			showCoin.isVisible = true
+			composer.gotoScene("home", options)
+		end
+	end
+	nextScript()
+
+
+	local function tap( event )
+		audio.play(SE1, {channel=5})
+		audio.stopWithDelay(420, {channel=5})
+		nextScript()
+	end
+	background:addEventListener("tap", tap)
+
+--[[
+	windowGroup:insert(background)
+	windowGroup:insert(skip_B)
+	windowGroup:insert(bubble)
+	windowGroup:insert(click)
+	windowGroup:insert(script)]]
+
+
+	-- 레이어 정리
+	sceneGroup:insert(background)
+	sceneGroup:insert(skip_B)
+	sceneGroup:insert(bubble)
+	--sceneGroup:insert(name)
+	sceneGroup:insert(click)
+	sceneGroup:insert(script)
+
+
+	--windowGroup:removeSelf()
+end
+
+function scene:show( event )
+	local sceneGroup = self.view
+	local phase = event.phase
+	
+	if phase == "will" then
+		-- Called when the scene is still off screen and is about to move on screen
+	elseif phase == "did" then
+		-- Called when the scene is now on screen
+		-- 
+		-- INSERT code here to make the scene come alive
+		-- e.g. start timers, begin animation, play audio, etc.
+	end	
+end
+
+function scene:hide( event )
+	local sceneGroup = self.view
+	local phase = event.phase
+	
+	if event.phase == "will" then
+		-- Called when the scene is on screen and is about to move off screen
+		--
+		-- INSERT code here to pause the scene
+		-- e.g. stop timers, stop animation, unload sounds, etc.)
+
+		--{**오프닝 때 초기화(정적변수기능없음) 통째로 가져가주세요--
+		--coinNum = 0 --}
+
+
+		--{**오프닝 때 초기화(정적변수기능없음) 통째로 가져가주세요--
+		exp = 0 --exp = 0
+		levelNum = 1 --levelNum = 1 
+
+		levelFirstTime = {} -- 경험치가 처음으로 일정 수준을 넘었을 때만 레벨업을 주기 위함. --
+			function levelTimeReset() 
+				for i=2, 10 do
+		 		levelFirstTime[i] = 1
+			end
+		end
+		levelTimeReset() --}
+
+		ingreCnt = {} --재료카운트
+		for i=1, 11 do
+			ingreCnt[i] = 0
+		end
+
+	elseif phase == "did" then
+
+		
+		-- Called when the scene is now off screen
+		composer.removeScene("intro")
+	end
+end
+
+function scene:destroy( event )
+	local sceneGroup = self.view
+	
+	-- Called prior to the removal of scene's "view" (sceneGroup)
+	-- 
+	-- INSERT code here to cleanup the scene
+	-- e.g. remove display objects, remove touch listeners, save state, etc.
+end
+
+---------------------------------------------------------------------------------
+
+-- Listener setup
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+
+-----------------------------------------------------------------------------------------
+
+return scene
