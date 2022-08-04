0b60766df015874ccc03b2f072f5f86863a8c7ca
jpg723 <86431761+jpg723@users.noreply.github.com>
2022-08-04 16:00:56 +0900

    재료 선택 칸 위치 변경

--- breadGame/material.lua
+++ breadGame/material.lua
@@ -73,119 +73,8 @@ function scene:create( event )
 	--재료 선택 칸--
 	local box2 = display.newImage("Content/image/box2.png", display.contentWidth*0.495, display.contentHeight*0.489)
 
-	--치즈--
-	local cheese = display.newImage("Content/image/cheese_btn.png", display.contentWidth * 0.2, display.contentHeight * 0.623)
-	--cheese.x, cheese.y = display.contentWidth * 0.2, display.contentHeight * 2.31
-
-	local function cheese_button( event )
- 		ingredient = 4
-	    if ( event.phase == "began" ) then
-	    	display.remove( material_image )
-	   		material_image = display.newImage("Content/image/cheese.png", display.contentWidth * 0.76, display.contentHeight * 0.51)
-	    	display.remove(material_cnt)
-	    	material_cnt = display.newText("보유 개수 : "..ingreCnt[8], display.contentWidth * 0.795, display.contentHeight * 0.484, "Content/font/ONE Mobile POP.ttf", 23)
-			material_cnt:setFillColor(0)
-	    	display.remove(material_text)
-	    	material_text = display.newText("치즈", display.contentWidth * 0.765, display.contentHeight * 0.562, "Content/font/ONE Mobile POP.ttf", 33.5)
-	    	material_text:setFillColor(0)
-	    	composer.setVariable("m", ingredient)
-			--material_image.x, material_image.y = display.contentWidth * 0.75, display.contentHeight * 0.61
-		end
-	end
- 
-	cheese:addEventListener( "touch", cheese_button )
-
-	--팥--
-	local bean = display.newImage("Content/image/bean_btn.png", display.contentWidth * 0.26, display.contentHeight * 0.623)
-	--bean.x, bean.y = display.contentWidth * 0.26, display.contentHeight * 2.308
-
-	local function bean_button( event )
- 		ingredient = 3
-	    if ( event.phase == "began" ) then
-	    	display.remove( material_image )
-	   		material_image = display.newImage("Content/image/bean.png", display.contentWidth * 0.76, display.contentHeight * 0.51)
-	    	display.remove(material_cnt)
-	    	material_cnt = display.newText("보유 개수 : "..ingreCnt[7], display.contentWidth * 0.795, display.contentHeight * 0.484, "Content/font/ONE Mobile POP.ttf", 23)
-			material_cnt:setFillColor(0)
-	    	display.remove(material_text)
-	    	material_text = display.newText("팥", display.contentWidth * 0.765, display.contentHeight * 0.561, "Content/font/ONE Mobile POP.ttf", 33.5)
-	    	material_text:setFillColor(0)
-	    	composer.setVariable("m", ingredient)
-			--material_image.x, material_image.y = display.contentWidth * 0.765, display.contentHeight * 0.61
-		end
-	end
- 
-	bean:addEventListener( "touch", bean_button )
-
-	--옥수수--
-	local corn = display.newImage("Content/image/corn_btn.png", display.contentWidth * 0.33, display.contentHeight * 0.623)
-	--corn.x, corn.y = display.contentWidth * 0.335, display.contentHeight * 2.3
-
-	local function corn_button( event )
- 		ingredient = 5 
-	    if ( event.phase == "began" ) then
-	    	display.remove( material_image )
-	   		material_image = display.newImage("Content/image/corn.png", display.contentWidth * 0.765, display.contentHeight * 0.51)
-	    	display.remove(material_cnt)
-	    	material_cnt = display.newText("보유 개수 : "..ingreCnt[9], display.contentWidth * 0.795, display.contentHeight * 0.484, "Content/font/ONE Mobile POP.ttf", 23)
-			material_cnt:setFillColor(0)
-	    	display.remove(material_text)
-	    	material_text = display.newText("옥수수", display.contentWidth * 0.765, display.contentHeight * 0.562, "Content/font/ONE Mobile POP.ttf", 33.5)
-	    	material_text:setFillColor(0)
-	    	composer.setVariable("m", ingredient)
-			--material_image.x, material_image.y = display.contentWidth * 0.765, display.contentHeight * 0.6
-		end
-	end
- 
-	corn:addEventListener( "touch", corn_button )
-
-	--슈가파우더--
-	local sugarpowder = display.newImage("Content/image/sugarpowder_btn.png", display.contentWidth * 0.45, display.contentHeight * 0.625)
-	--sugarpowder.x, sugarpowder.y = display.contentWidth * 0.45, display.contentHeight * 2.33
-
-	local function sugarpowder_button( event )
- 		ingredient = 7
-	    if ( event.phase == "began" ) then
-	    	display.remove( material_image )
-	   		material_image = display.newImage("Content/image/sugarpowder.png", display.contentWidth * 0.764, display.contentHeight * 0.515)
-	    	display.remove(material_cnt)
-	    	material_cnt = display.newText("보유 개수 : "..ingreCnt[11], display.contentWidth * 0.795, display.contentHeight * 0.484, "Content/font/ONE Mobile POP.ttf", 23)
-			material_cnt:setFillColor(0)
-	    	display.remove(material_text)
-	    	material_text = display.newText("슈가파우더", display.contentWidth * 0.765, display.contentHeight * 0.562, "Content/font/ONE Mobile POP.ttf", 33.5)
-	    	material_text:setFillColor(0)
-	    	composer.setVariable("m", ingredient)
-			--material_image.x, material_image.y = display.contentWidth * 0.761, display.contentHeight * 0.62
-		end
-	end
- 
-	sugarpowder:addEventListener( "touch", sugarpowder_button )
-
-	--소세지--
-	local sausage = display.newImage("Content/image/sausage_btn.png", display.contentWidth * 0.558, display.contentHeight * 0.624)
-	--sausage.x, sausage.y = display.contentWidth * 0.557, display.contentHeight * 2.31
-
-	local function sausage_button( event )
- 		ingredient = 2
- 
-	    if ( event.phase == "began" ) then
-	    	display.remove( material_image )
-	   		material_image = display.newImage("Content/image/sausage.png", display.contentWidth * 0.76, display.contentHeight * 0.515)
-	    	display.remove(material_cnt)
-	    	material_cnt = display.newText("보유 개수 : "..ingreCnt[6], display.contentWidth * 0.795, display.contentHeight * 0.484, "Content/font/ONE Mobile POP.ttf", 23)
-			material_cnt:setFillColor(0)
-	    	display.remove(material_text)
-	    	material_text = display.newText("소세지", display.contentWidth * 0.765, display.contentHeight * 0.562, "Content/font/ONE Mobile POP.ttf", 33.5)
-	    	material_text:setFillColor(0)
-	    	composer.setVariable("m", ingredient)
-			--material_image.x, material_image.y = display.contentWidth * 0.76, display.contentHeight * 0.65
-		end
-	end
- 
-	sausage:addEventListener( "touch", sausage_button )
-
 	--반짝이--
-	local glitter = display.newImage("Content/image/glitter_btn.png",  display.contentWidth * 0.65, display.contentHeight * 0.624)
+	local glitter = display.newImage("Content/image/glitter_btn.png",  display.contentWidth * 0.2, display.contentHeight * 0.625)
 	--glitter.x, glitter.y = display.contentWidth * 0.656, display.contentHeight * 2.31
 
 	local function glitter_button( event ) 
@@ -206,9 +95,98 @@ function scene:create( event )
 	end
  
 	glitter:addEventListener( "touch", glitter_button )
+	
+	--소세지--
+	local sausage = display.newImage("Content/image/sausage_btn.png", display.contentWidth * 0.295, display.contentHeight * 0.624)
+	--sausage.x, sausage.y = display.contentWidth * 0.557, display.contentHeight * 2.31
+
+	local function sausage_button( event )
+ 		ingredient = 2
+ 
+	    if ( event.phase == "began" ) then
+	    	display.remove( material_image )
+	   		material_image = display.newImage("Content/image/sausage.png", display.contentWidth * 0.76, display.contentHeight * 0.515)
+	    	display.remove(material_cnt)
+	    	material_cnt = display.newText("보유 개수 : "..ingreCnt[6], display.contentWidth * 0.795, display.contentHeight * 0.484, "Content/font/ONE Mobile POP.ttf", 23)
+			material_cnt:setFillColor(0)
+	    	display.remove(material_text)
+	    	material_text = display.newText("소세지", display.contentWidth * 0.765, display.contentHeight * 0.562, "Content/font/ONE Mobile POP.ttf", 33.5)
+	    	material_text:setFillColor(0)
+	    	composer.setVariable("m", ingredient)
+			--material_image.x, material_image.y = display.contentWidth * 0.76, display.contentHeight * 0.65
+		end
+	end
+ 
+	sausage:addEventListener( "touch", sausage_button )
+	
+	--팥--
+	local bean = display.newImage("Content/image/bean_btn.png", display.contentWidth * 0.37, display.contentHeight * 0.623)
+	--bean.x, bean.y = display.contentWidth * 0.26, display.contentHeight * 2.308
+
+	local function bean_button( event )
+ 		ingredient = 3
+	    if ( event.phase == "began" ) then
+	    	display.remove( material_image )
+	   		material_image = display.newImage("Content/image/bean.png", display.contentWidth * 0.76, display.contentHeight * 0.51)
+	    	display.remove(material_cnt)
+	    	material_cnt = display.newText("보유 개수 : "..ingreCnt[7], display.contentWidth * 0.795, display.contentHeight * 0.484, "Content/font/ONE Mobile POP.ttf", 23)
+			material_cnt:setFillColor(0)
+	    	display.remove(material_text)
+	    	material_text = display.newText("팥", display.contentWidth * 0.765, display.contentHeight * 0.561, "Content/font/ONE Mobile POP.ttf", 33.5)
+	    	material_text:setFillColor(0)
+	    	composer.setVariable("m", ingredient)
+			--material_image.x, material_image.y = display.contentWidth * 0.765, display.contentHeight * 0.61
+		end
+	end
+ 
+	bean:addEventListener( "touch", bean_button )
+
+	--치즈--
+	local cheese = display.newImage("Content/image/cheese_btn.png", display.contentWidth * 0.443, display.contentHeight * 0.623)
+	--cheese.x, cheese.y = display.contentWidth * 0.2, display.contentHeight * 2.31
+
+	local function cheese_button( event )
+ 		ingredient = 4
+	    if ( event.phase == "began" ) then
+	    	display.remove( material_image )
+	   		material_image = display.newImage("Content/image/cheese.png", display.contentWidth * 0.76, display.contentHeight * 0.51)
+	    	display.remove(material_cnt)
+	    	material_cnt = display.newText("보유 개수 : "..ingreCnt[8], display.contentWidth * 0.795, display.contentHeight * 0.484, "Content/font/ONE Mobile POP.ttf", 23)
+			material_cnt:setFillColor(0)
+	    	display.remove(material_text)
+	    	material_text = display.newText("치즈", display.contentWidth * 0.765, display.contentHeight * 0.562, "Content/font/ONE Mobile POP.ttf", 33.5)
+	    	material_text:setFillColor(0)
+	    	composer.setVariable("m", ingredient)
+			--material_image.x, material_image.y = display.contentWidth * 0.75, display.contentHeight * 0.61
+		end
+	end
+ 
+	cheese:addEventListener( "touch", cheese_button )
+
+	--옥수수--
+	local corn = display.newImage("Content/image/corn_btn.png", display.contentWidth * 0.53, display.contentHeight * 0.623)
+	--corn.x, corn.y = display.contentWidth * 0.335, display.contentHeight * 2.3
+
+	local function corn_button( event )
+ 		ingredient = 5 
+	    if ( event.phase == "began" ) then
+	    	display.remove( material_image )
+	   		material_image = display.newImage("Content/image/corn.png", display.contentWidth * 0.765, display.contentHeight * 0.51)
+	    	display.remove(material_cnt)
+	    	material_cnt = display.newText("보유 개수 : "..ingreCnt[9], display.contentWidth * 0.795, display.contentHeight * 0.484, "Content/font/ONE Mobile POP.ttf", 23)
+			material_cnt:setFillColor(0)
+	    	display.remove(material_text)
+	    	material_text = display.newText("옥수수", display.contentWidth * 0.765, display.contentHeight * 0.562, "Content/font/ONE Mobile POP.ttf", 33.5)
+	    	material_text:setFillColor(0)
+	    	composer.setVariable("m", ingredient)
+			--material_image.x, material_image.y = display.contentWidth * 0.765, display.contentHeight * 0.6
+		end
+	end
+ 
+	corn:addEventListener( "touch", corn_button )
 
 	--오레오파우더--
-	local oreopowder = display.newImage("Content/image/oreopowder_btn.png", display.contentWidth * 0.77, display.contentHeight * 0.622)
+	local oreopowder = display.newImage("Content/image/oreopowder_btn.png", display.contentWidth * 0.643, display.contentHeight * 0.621)
 	--oreopowder.x, oreopowder.y = display.contentWidth * 0.77, display.contentHeight * 2.27
 
 	local function oreopowder_button( event )
@@ -229,6 +207,27 @@ function scene:create( event )
  
 	oreopowder:addEventListener( "touch", oreopowder_button )
 
+	--슈가파우더--
+	local sugarpowder = display.newImage("Content/image/sugarpowder_btn.png", display.contentWidth * 0.775, display.contentHeight * 0.624)
+	--sugarpowder.x, sugarpowder.y = display.contentWidth * 0.45, display.contentHeight * 2.33
+
+	local function sugarpowder_button( event )
+ 		ingredient = 7
+	    if ( event.phase == "began" ) then
+	    	display.remove( material_image )
+	   		material_image = display.newImage("Content/image/sugarpowder.png", display.contentWidth * 0.764, display.contentHeight * 0.515)
+	    	display.remove(material_cnt)
+	    	material_cnt = display.newText("보유 개수 : "..ingreCnt[11], display.contentWidth * 0.795, display.contentHeight * 0.484, "Content/font/ONE Mobile POP.ttf", 23)
+			material_cnt:setFillColor(0)
+	    	display.remove(material_text)
+	    	material_text = display.newText("슈가파우더", display.contentWidth * 0.765, display.contentHeight * 0.562, "Content/font/ONE Mobile POP.ttf", 33.5)
+	    	material_text:setFillColor(0)
+	    	composer.setVariable("m", ingredient)
+			--material_image.x, material_image.y = display.contentWidth * 0.761, display.contentHeight * 0.62
+		end
+	end
+ 
+	sugarpowder:addEventListener( "touch", sugarpowder_button )
 	--굽기 버튼--
 	local start_button = display.newImage("Content/image/start.png", display.contentWidth*0.5, display.contentHeight*0.71)
 
